30cd38651b09af38c452b1477b66b399f9594446
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-17 06:06:02 +0600

    test

--- src/features/doctors-schedule/ui.tsx
+++ src/features/doctors-schedule/ui.tsx
@@ -5,10 +5,22 @@ export const DoctorsSchedule = () => {
   const example = DOCTORS_DETAILS_LIST[1].availabilityCalendar;
 
   return (
-    <div className='bg-white rounded-2xl px-4 py-5 my-4 grid grid-cols-4 gap-2 max-h-41.25 overflow-auto'>
-      {example.map((el) => (
-        <DoctorsScheduleItem key={el.date} data={el} />
-      ))}
+    <div className='bg-white rounded-2xlpy-5 my-4'>
+      <div className='grid grid-cols-4 gap-2 max-h-41.25 overflow-auto px-4 py-5'>
+        {example.map((el) => (
+          <DoctorsScheduleItem key={el.date} data={el} />
+        ))}
+      </div>
+      <div className='p-4 flex flex-wrap gap-3'>
+        {example[0].times.map((el) => (
+          <div
+            key={el}
+            className='w-18.75 h-6.5 flex justify-center items-center rounded-full bg-[#736767]'
+          >
+            <span className='text-white text-xs font-semibold'>{el}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
