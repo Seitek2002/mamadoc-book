68f68086259da26947a2929628abf7c769f79d1d
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-17 06:40:40 +0600

    changes

--- src/shared/ui/page-title/ui.tsx
+++ src/shared/ui/page-title/ui.tsx
@@ -1,4 +1,6 @@
-import Link from 'next/link';
+'use client';
+
+import { useRouter } from 'next/navigation';
 import ArrowIcon from '@/shared/assets/icons/arrow.svg';
 
 interface PageTitleProps {
@@ -6,18 +8,27 @@ interface PageTitleProps {
   href?: string;
 }
 
-export const PageTitle = ({ title, href = '/' }: PageTitleProps) => {
+export const PageTitle = ({ title, href }: PageTitleProps) => {
+  const router = useRouter();
+
   const shortTitle = title.includes(',') ? title.split(',')[0] : title;
 
+  const handleBack = (e: React.MouseEvent) => {
+    if (!href) {
+      e.preventDefault();
+      router.back();
+    }
+  };
+
   return (
-    <Link
-      href={href}
-      className='flex gap-1.5 items-center md:mt-6 select-none max-w-100'
+    <div
+      onClick={handleBack}
+      className='flex gap-1.5 items-center md:mt-6 select-none max-w-100 cursor-pointer active:opacity-70 transition-opacity'
     >
       <ArrowIcon className='shrink-0' />
 
       <h2 className='text-base font-medium lg:hidden'>{shortTitle}</h2>
       <h2 className='text-sm font-medium hidden lg:inline'>{title}</h2>
-    </Link>
+    </div>
   );
 };
