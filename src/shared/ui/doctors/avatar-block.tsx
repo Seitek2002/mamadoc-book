f7f83f16c4bfb0ae03ae21c25f7adf1a7eaba7e7
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-23 11:20:39 +0600

    add skeleton

--- src/shared/ui/doctors/avatar-block.tsx
+++ src/shared/ui/doctors/avatar-block.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { useState } from 'react';
 import Image, { StaticImageData } from 'next/image';
+import clsx from 'clsx';
 
 export const AvatarBlock = ({
   photo,
@@ -9,14 +13,25 @@ export const AvatarBlock = ({
   fullName: string;
   specialty: string;
 }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
-    <div className='w-32.25 relative lg:w-full lg:h-52.25 shrink-0 flex justify-center items-stretch'>
+    <div className='w-32.25 relative lg:w-full lg:h-52.25 shrink-0 flex justify-center items-stretch overflow-hidden bg-gray-100 rounded-lg'>
+      {!isLoaded && (
+        <div className='absolute inset-0 bg-gray-200 animate-pulse z-10' />
+      )}
+
       <Image
         src={photo}
         alt={fullName}
-        className='shrink-0 object-cover w-full h-full'
+        className={clsx(
+          'shrink-0 object-cover w-full h-full transition-opacity duration-300',
+          isLoaded ? 'opacity-100' : 'opacity-0',
+        )}
+        onLoad={() => setIsLoaded(true)}
       />
-      <span className='absolute right-0 bottom-0 px-2 py-1.5 rounded-tl-[10px] hidden lg:inline text-xs font-semibold text-gray bg-white'>
+
+      <span className='absolute right-0 bottom-0 px-2 py-1.5 rounded-tl-[10px] hidden lg:inline text-xs font-semibold text-gray bg-white z-20'>
         {specialty}
       </span>
     </div>
