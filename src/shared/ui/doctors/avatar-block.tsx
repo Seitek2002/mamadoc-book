15f18cfc35f43fc7c84a9b8197df948da2cf3175
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-23 11:22:39 +0600

    speed upped skeleton

--- src/shared/ui/doctors/avatar-block.tsx
+++ src/shared/ui/doctors/avatar-block.tsx
@@ -1,8 +1,4 @@
-'use client';
-
-import { useState } from 'react';
 import Image, { StaticImageData } from 'next/image';
-import clsx from 'clsx';
 
 export const AvatarBlock = ({
   photo,
@@ -13,25 +9,15 @@ export const AvatarBlock = ({
   fullName: string;
   specialty: string;
 }) => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
   return (
-    <div className='w-32.25 relative lg:w-full lg:h-52.25 shrink-0 flex justify-center items-stretch overflow-hidden bg-gray-100 rounded-lg'>
-      {!isLoaded && (
-        <div className='absolute inset-0 bg-gray-200 animate-pulse z-10' />
-      )}
-
+    <div className='w-32.25 relative lg:w-full lg:h-52.25 shrink-0 flex justify-center items-stretch'>
       <Image
         src={photo}
         alt={fullName}
-        className={clsx(
-          'shrink-0 object-cover w-full h-full transition-opacity duration-300',
-          isLoaded ? 'opacity-100' : 'opacity-0',
-        )}
-        onLoad={() => setIsLoaded(true)}
+        className='shrink-0 object-cover w-full h-full'
+        placeholder='blur' // Встроенный "скелетон" от Next.js
       />
-
-      <span className='absolute right-0 bottom-0 px-2 py-1.5 rounded-tl-[10px] hidden lg:inline text-xs font-semibold text-gray bg-white z-20'>
+      <span className='absolute right-0 bottom-0 px-2 py-1.5 rounded-tl-[10px] hidden lg:inline text-xs font-semibold text-gray bg-white'>
         {specialty}
       </span>
     </div>
