dfc2a2d5b298eb4004851314fda8f6044aef27df
Seitek2002 <seitekbegaliev@gmail.com>
2026-03-23 11:17:45 +0600

    refacr

--- src/app/doctor/[id]/BookingContainer.tsx
+++ src/app/doctor/[id]/BookingContainer.tsx
@@ -141,7 +141,7 @@ export function BookingWrapper({ id }: BookingWrapperProps) {
           <div className='hidden lg:flex justify-center w-full pt-4 pb-8'>
             <button
               onClick={handleBooking}
-              className='bg-[#007BFF] hover:bg-[#0069D9] font-medium text-white w-[308px] h-[41px] text-base rounded-full shadow-md active:scale-95 transition-all'
+              className='bg-[#007BFF] hover:bg-[#0069D9] font-medium text-white w-77 h-10.25 text-base rounded-full shadow-md active:scale-95 transition-all'
             >
               Записаться
             </button>
@@ -170,14 +170,14 @@ export function BookingWrapper({ id }: BookingWrapperProps) {
       </div>
 
       {/* Плашка с датой — МОБИЛЬНАЯ (Скрыта на ПК через lg:hidden) */}
-      <div className='fixed lg:hidden left-1/2 font-semibold -translate-x-1/2 rounded-full bottom-15 shadow-2xl flex justify-center items-center text-sm h-[30px] w-[147px] bg-[#FAF9F9] z-10 border border-white'>
+      <div className='fixed lg:hidden left-1/2 font-semibold -translate-x-1/2 rounded-full bottom-15 shadow-2xl flex justify-center items-center text-sm h-7.5 w-36.75 bg-[#FAF9F9] z-10 border border-white'>
         13 марта, 13:00
       </div>
 
       {/* МОБИЛЬНАЯ КНОПКА ЗАПИСИ (Скрыта на ПК через lg:hidden) */}
       <button
         onClick={handleBooking}
-        className='fixed lg:hidden left-1/2 -translate-x-1/2 bottom-4 text-sm bg-[#007BFF] hover:bg-[#0069D9] font-medium text-white w-[203px] md:w-[80%] max-w-200 py-2.5 rounded-full shadow-xl active:scale-95 transition-all z-40'
+        className='fixed lg:hidden left-1/2 -translate-x-1/2 bottom-4 text-sm bg-[#007BFF] hover:bg-[#0069D9] font-medium text-white w-50.75 md:w-[80%] max-w-200 py-2.5 rounded-full shadow-xl active:scale-95 transition-all z-40'
       >
         Записаться
       </button>
